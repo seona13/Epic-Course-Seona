277a26f86d8a801002b3a798da5b12d78acd905a
Seona Bellamy <seona13@gmail.com>
2021-02-17 08:31:11 +1100

    Turn the SpawnManager into a singleton
    Had to create it first, but then what it says on the tin.

---
+++ Assets/Epic_Tower_Defense/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnManager : MonoBehaviour
+{
+	private static SpawnManager _Instance;
+	public static SpawnManager Instance
+	{
+		get
+		{
+			if (_Instance == null)
+			{
+				var obj = new GameObject().AddComponent<SpawnManager>();
+				obj.name = "Spawn_Manager";
+				_Instance = obj.GetComponent<SpawnManager>();
+			}
+			return _Instance;
+		}
+	}
+
+
+
+	void Awake()
+	{
+		if (_Instance != null)
+		{
+			Debug.LogWarning("Second instance of SpawnManager created. Automatic self-destruct triggered.");
+			Destroy(this.gameObject);
+		}
+	}
+
+
+	private void OnDestroy()
+	{
+		if (_Instance == this)
+		{
+			_Instance = null;
+		}
+	}
+}
