aef48c56945008cdbe60d8c2c6a2237de5518652
Seona Bellamy <seona13@gmail.com>
2021-02-16 14:24:04 +1100

    Enemies walk the floor
    * Created AI script on enemies.
    * Created Inlet and Outlet points and linked the outlet to enemies.
    * Enemies walk towards Outlet.
    * Rebaked NavMesh to properly avoid rocky areas, including adding a couple of extra large rocks to block lane ends.

---
+++ Assets/Epic_Tower_Defense/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAI : MonoBehaviour
+{
+	private NavMeshAgent _agent;
+	[SerializeField]
+	private Transform _outlet;
+
+
+
+	void Start()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+		if (_agent == null)
+		{
+			Debug.LogError("Missing NavMeshAgent on enemy");
+		}
+		_agent.SetDestination(_outlet.position);
+		Debug.Log(_outlet.position);
+	}
+
+
+	void Update()
+	{
+
+	}
+}
