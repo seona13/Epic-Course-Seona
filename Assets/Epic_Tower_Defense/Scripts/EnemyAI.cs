447d495b85411cb558390df4bf8fea739f4e23b7
Seona Bellamy <seona13@gmail.com>
2021-02-16 14:37:45 +1100

    Enemy script updates
    * Enemy moves at 1.5m/sec (this is already customisable in the inspector directly on the NavMeshAgent, so I couldn't see any value in building it into the script at this point.)
    
    * Enemy has a MaxHealth value and a CurrentHealth value. When the enemy spawns, CurrentHealth is set to MaxHealth.
    
    * Enemy has a KillValue that represents the reward (in currency, points, or whatever) the player gets for killing them.

--- Assets/Epic_Tower_Defense/Scripts/EnemyAI.cs
+++ Assets/Epic_Tower_Defense/Scripts/EnemyAI.cs
@@ -9,7 +9,12 @@ public class EnemyAI : MonoBehaviour
 	private NavMeshAgent _agent;
 	[SerializeField]
 	private Transform _outlet;
-
+	[SerializeField]
+	private int _maxHealth = 200;
+	[SerializeField]
+	private int _currentHealth;
+	[SerializeField]
+	private int _killValue = 50;
 
 
 	void Start()
@@ -20,7 +25,9 @@ public class EnemyAI : MonoBehaviour
 			Debug.LogError("Missing NavMeshAgent on enemy");
 		}
 		_agent.SetDestination(_outlet.position);
-		Debug.Log(_outlet.position);
+
+		// Give enemy full health on spawn.
+		_currentHealth = _maxHealth;
 	}
 
 
