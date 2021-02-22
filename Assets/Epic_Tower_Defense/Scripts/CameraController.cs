94251c6f46f4d3a6aa3390da518496ed7acdb50e
Seona Bellamy <seona13@gmail.com>
2021-02-23 08:41:01 +1100

    Camera Zoom
    * Camera can now be zoomed in and out with the mouse wheel, with sensible minimum and maximum distances.

---
+++ Assets/Epic_Tower_Defense/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField]
+	private float _zoomSpeed = 10f;
+
+	private Camera _cameraComp;
+
+
+
+	void Start()
+	{
+		_cameraComp = GetComponent<Camera>();
+	}
+
+
+	void Update()
+	{
+		if (Input.mouseScrollDelta.y != 0)
+		{
+			_cameraComp.fieldOfView += -Input.mouseScrollDelta.y * _zoomSpeed * Time.deltaTime;
+			_cameraComp.fieldOfView = Mathf.Clamp(_cameraComp.fieldOfView, 20, 40);
+		}
+	}
+}
