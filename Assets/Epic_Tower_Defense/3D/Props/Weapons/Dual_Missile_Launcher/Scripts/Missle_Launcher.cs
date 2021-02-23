cb7814123023277d576314b270db87bdb13015e7
Seona Bellamy <seona13@gmail.com>
2021-02-24 09:03:18 +1100

    ITower interface
    * Created an interface to handle features common to all towers.
    
    * Extended all 4 tower types to use the new interface.
    
    * Also a bit of file structure tidying in the Scripts folder.

--- Assets/Epic_Tower_Defense/3D/Props/Weapons/Dual_Missile_Launcher/Scripts/Missle_Launcher.cs
+++ Assets/Epic_Tower_Defense/3D/Props/Weapons/Dual_Missile_Launcher/Scripts/Missle_Launcher.cs
@@ -1,78 +1,84 @@
-﻿using System.Collections;
+﻿using GameDevHQ.FileBase.Missle_Launcher_Dual_Turret.Missle;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GameDevHQ.FileBase.Missle_Launcher_Dual_Turret.Missle;
 
 namespace GameDevHQ.FileBase.Missle_Launcher_Dual_Turret
 {
-    public class Missle_Launcher : MonoBehaviour
-    {
-        [SerializeField]
-        private GameObject _missilePrefab; //holds the missle gameobject to clone
-        [SerializeField]
-        private GameObject[] _misslePositionsLeft; //array to hold the rocket positions on the turret
-        [SerializeField]
-        private GameObject[] _misslePositionsRight; //array to hold the rocket positions on the turret
-        [SerializeField]
-        private float _fireDelay; //fire delay between rockets
-        [SerializeField]
-        private float _launchSpeed; //initial launch speed of the rocket
-        [SerializeField]
-        private float _power; //power to apply to the force of the rocket
-        [SerializeField]
-        private float _fuseDelay; //fuse delay before the rocket launches
-        [SerializeField]
-        private float _reloadTime; //time in between reloading the rockets
-        [SerializeField]
-        private float _destroyTime = 10.0f; //how long till the rockets get cleaned up
-        private bool _launched; //bool to check if we launched the rockets
+	public class Missle_Launcher : MonoBehaviour, ITower
+	{
+		[SerializeField]
+		private GameObject _missilePrefab; //holds the missle gameobject to clone
+		[SerializeField]
+		private GameObject[] _misslePositionsLeft; //array to hold the rocket positions on the turret
+		[SerializeField]
+		private GameObject[] _misslePositionsRight; //array to hold the rocket positions on the turret
+		[SerializeField]
+		private float _fireDelay; //fire delay between rockets
+		[SerializeField]
+		private float _launchSpeed; //initial launch speed of the rocket
+		[SerializeField]
+		private float _power; //power to apply to the force of the rocket
+		[SerializeField]
+		private float _fuseDelay; //fuse delay before the rocket launches
+		[SerializeField]
+		private float _reloadTime; //time in between reloading the rockets
+		[SerializeField]
+		private float _destroyTime = 10.0f; //how long till the rockets get cleaned up
+		private bool _launched; //bool to check if we launched the rockets
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space) && _launched == false) //check for space key and if we launched the rockets
-            {
-                _launched = true; //set the launch bool to true
-                StartCoroutine(FireRocketsRoutine()); //start a coroutine that fires the rockets. 
-            }
-        }
+		#region ITower implementation
+		public int BuyFor { get; set; } = 500;
 
-        IEnumerator FireRocketsRoutine()
-        {
-            for (int i = 0; i < _misslePositionsLeft.Length; i++) //for loop to iterate through each missle position
-            {
-                GameObject rocketLeft = Instantiate(_missilePrefab) as GameObject; //instantiate a rocket
-                GameObject rocketRight = Instantiate(_missilePrefab) as GameObject; //instantiate a rocket
+		public int SellFor { get; set; } = 250;
+		#endregion
 
-                rocketLeft.transform.parent = _misslePositionsLeft[i].transform; //set the rockets parent to the missle launch position 
-                rocketRight.transform.parent = _misslePositionsRight[i].transform; //set the rockets parent to the missle launch position 
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space) && _launched == false) //check for space key and if we launched the rockets
+			{
+				_launched = true; //set the launch bool to true
+				StartCoroutine(FireRocketsRoutine()); //start a coroutine that fires the rockets. 
+			}
+		}
 
-                rocketLeft.transform.localPosition = Vector3.zero; //set the rocket position values to zero
-                rocketRight.transform.localPosition = Vector3.zero; //set the rocket position values to zero
+		IEnumerator FireRocketsRoutine()
+		{
+			for (int i = 0; i < _misslePositionsLeft.Length; i++) //for loop to iterate through each missle position
+			{
+				GameObject rocketLeft = Instantiate(_missilePrefab) as GameObject; //instantiate a rocket
+				GameObject rocketRight = Instantiate(_missilePrefab) as GameObject; //instantiate a rocket
 
-                rocketLeft.transform.localEulerAngles = new Vector3(0, 0, 0); //set the rotation values to be properly aligned with the rockets forward direction
-                rocketRight.transform.localEulerAngles = new Vector3(0, 0, 0); //set the rotation values to be properly aligned with the rockets forward direction
+				rocketLeft.transform.parent = _misslePositionsLeft[i].transform; //set the rockets parent to the missle launch position 
+				rocketRight.transform.parent = _misslePositionsRight[i].transform; //set the rockets parent to the missle launch position 
 
-                rocketLeft.transform.parent = null; //set the rocket parent to null
-                rocketRight.transform.parent = null; //set the rocket parent to null
+				rocketLeft.transform.localPosition = Vector3.zero; //set the rocket position values to zero
+				rocketRight.transform.localPosition = Vector3.zero; //set the rocket position values to zero
 
-                rocketLeft.GetComponent<GameDevHQ.FileBase.Missle_Launcher_Dual_Turret.Missle.Missle>().AssignMissleRules(_launchSpeed, _power, _fuseDelay, _destroyTime); //assign missle properties 
-                rocketRight.GetComponent<GameDevHQ.FileBase.Missle_Launcher_Dual_Turret.Missle.Missle>().AssignMissleRules(_launchSpeed, _power, _fuseDelay, _destroyTime); //assign missle properties 
+				rocketLeft.transform.localEulerAngles = new Vector3(0, 0, 0); //set the rotation values to be properly aligned with the rockets forward direction
+				rocketRight.transform.localEulerAngles = new Vector3(0, 0, 0); //set the rotation values to be properly aligned with the rockets forward direction
 
-                _misslePositionsLeft[i].SetActive(false); //turn off the rocket sitting in the turret to make it look like it fired
-                _misslePositionsRight[i].SetActive(false); //turn off the rocket sitting in the turret to make it look like it fired
+				rocketLeft.transform.parent = null; //set the rocket parent to null
+				rocketRight.transform.parent = null; //set the rocket parent to null
 
-                yield return new WaitForSeconds(_fireDelay); //wait for the firedelay
-            }
+				rocketLeft.GetComponent<GameDevHQ.FileBase.Missle_Launcher_Dual_Turret.Missle.Missle>().AssignMissleRules(_launchSpeed, _power, _fuseDelay, _destroyTime); //assign missle properties 
+				rocketRight.GetComponent<GameDevHQ.FileBase.Missle_Launcher_Dual_Turret.Missle.Missle>().AssignMissleRules(_launchSpeed, _power, _fuseDelay, _destroyTime); //assign missle properties 
 
-            for (int i = 0; i < _misslePositionsLeft.Length; i++) //itterate through missle positions
-            {
-                yield return new WaitForSeconds(_reloadTime); //wait for reload time
-                _misslePositionsLeft[i].SetActive(true); //enable fake rocket to show ready to fire
-                _misslePositionsRight[i].SetActive(true); //enable fake rocket to show ready to fire
-            }
+				_misslePositionsLeft[i].SetActive(false); //turn off the rocket sitting in the turret to make it look like it fired
+				_misslePositionsRight[i].SetActive(false); //turn off the rocket sitting in the turret to make it look like it fired
 
-            _launched = false; //set launch bool to false
-        }
-    }
+				yield return new WaitForSeconds(_fireDelay); //wait for the firedelay
+			}
+
+			for (int i = 0; i < _misslePositionsLeft.Length; i++) //itterate through missle positions
+			{
+				yield return new WaitForSeconds(_reloadTime); //wait for reload time
+				_misslePositionsLeft[i].SetActive(true); //enable fake rocket to show ready to fire
+				_misslePositionsRight[i].SetActive(true); //enable fake rocket to show ready to fire
+			}
+
+			_launched = false; //set launch bool to false
+		}
+	}
 }
 
---
+++ Assets/Epic_Tower_Defense/Scripts/Interfaces/ITower.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public interface ITower
+{
+	int BuyFor { get; set; }
+	int SellFor { get; set; }
+}
