43178a47272a0071e09cc742f692b98af896d51b
Seona Bellamy <seona13@gmail.com>
2021-02-25 13:30:11 +1100

    CameraController improvements
    * Cached min/max values for Clamps (currently unserialised because I'm done playing with them, but they could get serialised easily enough).
    
    * Broke big long clamped panning Vector3 out to make it more readable.
    
    * Broke zooming and panning into their own methods which get called from Update, just to make it easier to see which code belongs to which functionality.

--- Assets/Epic_Tower_Defense/Scripts/Controllers/CameraController.cs
+++ Assets/Epic_Tower_Defense/Scripts/Controllers/CameraController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraController : MonoBehaviour
 {
 	[SerializeField]
@@ -10,6 +11,13 @@ public class CameraController : MonoBehaviour
 	private float _panSpeed = 10f;
 	[SerializeField]
 	private float _panBorder = 25f;
+
+	private float _zoomMin = 20f;
+	private float _zoomMax = 40f;
+	private float _panXMin = -40f;
+	private float _panXMax = -20f;
+	private float _panZMin = -25f;
+	private float _panZMax = -7f;
 
 	private Camera _cameraComp;
 	private GameObject _parent;
@@ -25,12 +33,23 @@ public class CameraController : MonoBehaviour
 
 	void Update()
 	{
+		DoZoom();
+		DoPan();
+	}
+
+
+	void DoZoom()
+	{
 		if (Input.mouseScrollDelta.y != 0)
 		{
 			_cameraComp.fieldOfView += -Input.mouseScrollDelta.y * _zoomSpeed * Time.deltaTime;
-			_cameraComp.fieldOfView = Mathf.Clamp(_cameraComp.fieldOfView, 20, 40);
+			_cameraComp.fieldOfView = Mathf.Clamp(_cameraComp.fieldOfView, _zoomMin, _zoomMax);
 		}
+	}
 
+
+	void DoPan()
+	{
 		float horizontalInput = Input.GetAxis("Horizontal");
 		if (Input.mousePosition.x < _panBorder)
 		{
@@ -54,7 +73,11 @@ public class CameraController : MonoBehaviour
 		if (horizontalInput != 0 || verticalInput != 0)
 		{
 			_parent.transform.Translate(new Vector3(horizontalInput, 0, verticalInput) * _panSpeed * Time.deltaTime);
-			_parent.transform.position = new Vector3(Mathf.Clamp(_parent.transform.position.x, -40, -20), _parent.transform.position.y, Mathf.Clamp(_parent.transform.position.z, -25, -7));
+			Vector3 newPos = new Vector3();
+			newPos.x = Mathf.Clamp(_parent.transform.position.x, _panXMin, _panXMax);
+			newPos.y = _parent.transform.position.y;
+			newPos.z = Mathf.Clamp(_parent.transform.position.z, _panZMin, _panZMax);
+			_parent.transform.position = newPos;
 		}
 	}
 }
